1292240fea189d9aa575f7491c2bf9fbe3799e72
Kritarth_IIITD <89209652+Kritarth384@users.noreply.github.com>
2021-12-16 23:31:45 +0530

    Create Main.java

---
+++ Ludo Game/Main.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+public class Main2 extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("New_form.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),640,480);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
