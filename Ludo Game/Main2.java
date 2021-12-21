7e8cb8ee9708ec8bdacda42a03a6933db285d036
srishti20409 <srishti20409@iiitd.ac.in>
2021-12-21 16:54:08 +0530

    stars added
    stars added

--- Ludo Game/Main2.java
+++ Ludo Game/Main2.java
@@ -4,13 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+
 public class Main2 extends Application {
 
     @Override
     public void start(Stage stage) throws IOException{
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("New_form.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main2.class.getResource("New_form.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),640,480);
         stage.setTitle("Hello!");
         stage.setScene(scene);
